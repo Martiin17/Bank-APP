2b5de67217016b258c38bc6f3f3c069ad1d7e533
Martiin17 <103968095+Martiin17@users.noreply.github.com>
2024-10-21 20:20:10 +0000

    Se agregan escenarios gerkhin y se comentan los no implementados

--- MeMo1-Ejercicio1/app/bin/test/memo1/ejercicio1/account-creation.feature
+++ MeMo1-Ejercicio1/app/bin/test/memo1/ejercicio1/account-creation.feature
@@ -1,20 +1,18 @@
 Feature: Account creation
   Scenario: Successfully create an account with default balance
-    Given I create an account with a owner, CBU 123456789 and alias hellow12
-    Then The account balance should be 0.0
+    Given I create an account with CBU 111111111 and alias "test0"
+    Then The account with CBU 111111111 should be have a balance of 0.0
 
   Scenario: Successfully create an account with an initial balance
-    Given I create an account with a owner, CBU 123456789, alias hellow12 and a balance of 1000.0
-    Then The account balance should be 1000.0
+    Given I create an account with CBU 123456789, alias hellow12 and a balance of 1000.0
+    Then The account with CBU 123456789 should be have a balance of 1000.0
 
   Scenario: Cant create an account with a repeat CBU
-    Given I create an account with a owner, CBU 123456789, alias hellow12 and a balance of 1000.0
-    When I try to create another account with the same owner, CBU 123456789 and a balance of 1000.0
-    Then The first account should be create with a balance of 1000.0
-    And The operation should be denied due to repeat CBU
+    Given I create an account with CBU 123456789, alias hellow12 and a balance of 1000.0
+    When I try to create another account with the same CBU 123456789 and a balance of 1000.0
+    Then The operation should be denied due to repeat CBU
 
   Scenario: Cant create an account with a repeat alias
-    Given I create an account with a owner, CBU 123456789, alias hellow12 and a balance of 1000.0
-    When I try to create another account with the same owner alias hellow12 and a balance of 1000.0
-    Then The first account should be create with a balance of 1000.0
-    And The operation should be denied due to repeat alias
+    Given I create an account with CBU 123456789, alias hellow12 and a balance of 1000.0
+    When I try to create another account with the same alias hellow12 and a balance of 1000.0
+    Then The operation should be denied due to repeat alias
--- MeMo1-Ejercicio1/app/src/test/java/memo1/ejercicio1/AccountSteps.java
+++ MeMo1-Ejercicio1/app/src/test/java/memo1/ejercicio1/AccountSteps.java
@@ -1,35 +1,58 @@
 package memo1.ejercicio1;
 
 import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
 import io.cucumber.java.en.*;
 
 // Pruebas funcionales basadas en los escenarios Gherkin
 
 public class AccountSteps {
-    private Account account;
+    //private Account account0;
+    private Account account1;
     private Account account2;
     private boolean operationResult;
-    private Branch sucursal1;
+    private Branch branch1;
     private Bank bank;
-}
+    private Client client1;
+    private Client client2;
+    private IllegalArgumentException iae;
 
-    /*@Given("I create an account with CBU {long}")
-    public void createAccountWithDefaultBalance(long cbu) {
-        account = new Account();
-        account.setCbu(cbu);
+    @BeforeEach
+    void setUp() {
+        bank = new Bank();
+        bank.createBranch(1, "Street 15", "branch1");
+        bank.CreateClient(12345, "Math", "Johnson",  "Street 14", 19900413);
+        bank.CreateClient(56789, "Kamala", "Harrison", "Street 14", 19911013);
+        branch1 = bank.getBranch(1);
+        client1 = bank.getClient(12345);
+        client2 = bank.getClient(56789);
+        //client1.createAccountAsTitular(bank, bank.getBranch(branch1.getBranchNumber()), 111111111L, "test0");
+        //client1.createAccountAsTitular(bank, bank.getBranch(branch1.getBranchNumber()), 123456789L, 1000.0, "hellow12");
+        //client2.createAccountAsTitular(bank, bank.getBranch(branch1.getBranchNumber()), 987654321L, 1000.0, "bye14");
+        //account1 = bank.getAccountByCBU(123456789);
+        //account2 = bank.getAccountByCBU(987654321L);
     }
 
-    @Given("I create an account with CBU {long} and a balance of {double}")
-    public void createAccountWithInitialBalance(long cbu, double balance) {
-        account = new Account(cbu, balance);
+
+    @Given("I create an account with CBU {long} and alias {string}")
+    public void createAccountWithDefaultBalance(long cbu, String alias) {
+        client1.createAccountAsTitular(bank, bank.getBranch(branch1.getBranchNumber()), cbu, alias);
     }
 
-    @Given("An account with CBU {long} and a balance of {double}")
+
+    @Given("I create an account with CBU {long}, alias {string} and a balance of {double}")
+    public void createAccountWithInitialBalance(long cbu, String alias, double balance) {
+        client1.createAccountAsTitular(bank, bank.getBranch(branch1.getBranchNumber()), cbu, balance, alias);
+    }
+
+    /*@Given("An account with CBU {long} and a balance of {double}")
     public void anAccountWithCBUAndBalance(long cbu, double balance) {
         account = new Account(cbu, balance);
-    }
+    }*/
 
-    @Given("An account with CBU {long} and a balance of {double} and a second account with CBU {long} and balance of {double}")
+    /*@Given("An account with CBU {long} and a balance of {double} and a second account with CBU {long} and balance of {double}")
     public void createAccountsWithCBUAndBalance(long cbu, Double balance, long cbu2, Double balance2) {
         account = new Account(cbu, balance);
         account2 = new Account(cbu2, balance2);
@@ -40,9 +63,9 @@ public class AccountSteps {
         sucursal1.addAccount(account2);
         account.setBank(bank);
         account2.setBank(bank);
-    }
+    }*/
 
-    @Given("An account with CBU {int} and a balance of {double} and a inexistent cbu {long}")
+    /*@Given("An account with CBU {int} and a balance of {double} and a inexistent cbu {long}")
     public void createAccountsWithCBUAndBalance(long cbu, Double balance, long inexistentCbu){
         account = new Account(cbu, balance);
         sucursal1 = new Sucursal(1, "PrimeraJunta  829", "sucursal1");
@@ -50,57 +73,88 @@ public class AccountSteps {
         bank.addSucursal(sucursal1);
         sucursal1.addAccount(account);
         account.setBank(bank);
+    }*/
+
+    @When("I try to create another account with the same CBU {long} and a balance of {double}")
+    public void depositIntoAccount(double balance, long cbu) {
+        try{
+            client1.createAccountAsTitular(bank, branch1, cbu, balance, "Dont12");
+        }catch(IllegalArgumentException iae){
+            this.iae = iae;
+        }
+       
     }
 
-    @When("I deposit {double} into the account")
+    @When("I try to create another account with the same alias {string} and a balance of {double}")
+    public void depositIntoAccount(double balance,String alias) {
+        try{
+            client1.createAccountAsTitular(bank, branch1, 231321314L, balance,alias);
+        }catch(IllegalArgumentException iae){
+            this.iae = iae;
+        }
+       
+    }
+
+   /*@When("I deposit {double} into the account")
     public void depositIntoAccount(double amount) {
         operationResult = account.deposit(amount);
-    }
+    }*/
 
-    @When("I try to deposit {double} into the account")
+    /*@When("I try to deposit {double} into the account")
     public void tryToDepositIntoAccount(double amount) {
         operationResult = account.deposit(amount);
-    }
+    }*/
 
-    @When("I withdraw {double} from the account")
+    /*@When("I withdraw {double} from the account")
     public void withdrawFromAccount(double amount) {
         operationResult = account.withdraw(amount);
-    }
+    }*/
 
-    @When("I try to withdraw {double} from the account")
+    /*@When("I try to withdraw {double} from the account")
     public void tryToWithdrawFromAccount(double amount) {
         operationResult = account.withdraw(amount);
-    }
+    }*/
 
-    @When("I transfer {double} into the second account")
+    /*@When("I transfer {double} into the second account")
     public void depositToSecondAccount(Double amount) {
         account.transferWithCBU(amount, account2.getCbu());
-    }
+    }*/
 
-    @When("I try to transfer {double} into the second account")
+   /*@When("I try to transfer {double} into the second account")
     public void tryToDepositIntoSecondAccount(Double amount) {
         account.transferWithCBU(amount, account2.getCbu());
-    }
+    }*/
 
-    @When("I try to transfer {double} to the cbu {long}")
+    /*@When("I try to transfer {double} to the cbu {long}")
     public void tryToTransferToInexistentCBU(Double amount, long inexistentCbu) {
         operationResult = account.transferWithCBU(amount, inexistentCbu);;
+    }*/
+
+    @Then("The operation should be denied due to repeat CBU")
+    public void verifyRepeatCBU() {
+        assertNotNull(iae);
     }
 
-    @Then("The account balance should be {double}")
-    public void verifyAccountBalance(double expectedBalance) {
+    @Then("The operation should be denied due to repeat alias")
+    public void verifyRepeatAlias() {
+        assertNotNull(iae);
+    }
+
+    @Then("The account with CBU {long} should be have a balance of {double}")
+    public void verifyAccountBalance(long cbu, double expectedBalance) {
+        Account account = client1.getOwnerAccountWithCBU(cbu);
         assertEquals(expectedBalance, account.getBalance(), 0.01);
     }
 
-    @Then("The operation should be denied due to negative amount")
+    /*@Then("The operation should be denied due to negative amount")
     public void verifyOperationDenied() {
         if(!operationResult) {
             System.out.println("negative amount");
         }
         assertFalse(operationResult);
-    }
+    }*/
 
-    @Then("The account balance should remain {double}")
+    /*@Then("The account balance should remain {double}")
     public void verifyBalanceRemains(double expectedBalance) {
         assertEquals(expectedBalance, account.getBalance(), 0.01);
     }
@@ -137,6 +191,6 @@ public class AccountSteps {
     @Then("The first account balance should remain {double}")
     public void VerifyAccountBalance(Double expectedBalance) {
         assertEquals(expectedBalance, account.getBalance(), 0.01);
-    }
+    }*/
 
-} */
+} 
